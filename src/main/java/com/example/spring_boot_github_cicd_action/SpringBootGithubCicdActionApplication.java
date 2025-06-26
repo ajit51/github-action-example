3085d9284e9acb6d5e011694dfec8c26753d118a
ajit51 <ajitk433@gmail.com>
2025-06-26 12:53:40 +0530

    first commit

---
+++ src/main/java/com/example/spring_boot_github_cicd_action/SpringBootGithubCicdActionApplication.java
@@ -0,0 +1,21 @@
+package com.example.spring_boot_github_cicd_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootGithubCicdActionApplication {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Github CICD Action Pipeline";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGithubCicdActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_boot_github_cicd_action/SpringBootGithubCicdActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_github_cicd_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGithubCicdActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
